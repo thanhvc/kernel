510adfdf197f053d5d9518485d058c1e22df0c74
Anatoliy Bazko <abazko@exoplatform.com>
2013-01-15 10:30:22 +0200

    KER-227: Upgrade the servlet api to 3.0

--- exo.kernel.container/src/main/java/org/exoplatform/test/MockServletContext.java
+++ exo.kernel.container/src/main/java/org/exoplatform/test/MockServletContext.java
@@ -23,16 +23,24 @@ import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Enumeration;
+import java.util.EventListener;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.Vector;
 
+import javax.servlet.Filter;
+import javax.servlet.FilterRegistration;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.Servlet;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+import javax.servlet.ServletRegistration.Dynamic;
+import javax.servlet.SessionCookieConfig;
+import javax.servlet.SessionTrackingMode;
+import javax.servlet.descriptor.JspConfigDescriptor;
 
 /**
  * Created by The eXo Platform SARL . Author : Mestrallet Benjamin
@@ -94,12 +102,12 @@ public class MockServletContext implements ServletContext
 
    public int getMajorVersion()
    {
-      return 2;
+      return 3;
    }
 
    public int getMinorVersion()
    {
-      return 5;
+      return 0;
    }
 
    public String getMimeType(String s)
@@ -191,14 +199,15 @@ public class MockServletContext implements ServletContext
       return null;
    }
 
-   public void setInitParameter(String name, String value)
+   public boolean setInitParameter(String name, String value)
    {
       initParams_.put(name, value);
+      return true;
    }
 
    public String getInitParameter(String name)
    {
-      return (String)initParams_.get(name);
+      return initParams_.get(name);
    }
 
    public Enumeration getInitParameterNames()
@@ -236,5 +245,129 @@ public class MockServletContext implements ServletContext
    public String getContextPath()
    {
       return contextPath_;
+   }
+
+   // servlet 3.0 API
+
+   public int getEffectiveMajorVersion()
+   {
+      return 3;
+   }
+
+   public int getEffectiveMinorVersion()
+   {
+      return 0;
+   }
+
+   public Dynamic addServlet(String servletName, String className)
+   {
+      return null;
+   }
+
+   public Dynamic addServlet(String servletName, Servlet servlet)
+   {
+      return null;
+   }
+
+   public Dynamic addServlet(String servletName, Class<? extends Servlet> servletClass)
+   {
+      return null;
+   }
+
+   public <T extends Servlet> T createServlet(Class<T> clazz) throws ServletException
+   {
+      return null;
+   }
+
+   public ServletRegistration getServletRegistration(String servletName)
+   {
+      return null;
+   }
+
+   public Map<String, ? extends ServletRegistration> getServletRegistrations()
+   {
+      return null;
+   }
+
+   public javax.servlet.FilterRegistration.Dynamic addFilter(String filterName, String className)
+   {
+      return null;
+   }
+
+   public javax.servlet.FilterRegistration.Dynamic addFilter(String filterName, Filter filter)
+   {
+      return null;
+   }
+
+   public javax.servlet.FilterRegistration.Dynamic addFilter(String filterName, Class<? extends Filter> filterClass)
+   {
+      return null;
+   }
+
+   public <T extends Filter> T createFilter(Class<T> clazz) throws ServletException
+   {
+      return null;
+   }
+
+   public FilterRegistration getFilterRegistration(String filterName)
+   {
+      return null;
+   }
+
+   public Map<String, ? extends FilterRegistration> getFilterRegistrations()
+   {
+      return null;
+   }
+
+   @Override
+   public SessionCookieConfig getSessionCookieConfig()
+   {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   public void setSessionTrackingModes(Set<SessionTrackingMode> sessionTrackingModes)
+   {
+   }
+
+   public Set<SessionTrackingMode> getDefaultSessionTrackingModes()
+   {
+      return null;
+   }
+
+   public Set<SessionTrackingMode> getEffectiveSessionTrackingModes()
+   {
+      return null;
+   }
+
+   public void addListener(String className)
+   {
+   }
+
+   public <T extends EventListener> void addListener(T t)
+   {
+   }
+
+   public void addListener(Class<? extends EventListener> listenerClass)
+   {
+   }
+
+   public <T extends EventListener> T createListener(Class<T> clazz) throws ServletException
+   {
+      return null;
+   }
+
+   public JspConfigDescriptor getJspConfigDescriptor()
+   {
+      return null;
+   }
+
+   public ClassLoader getClassLoader()
+   {
+      return null;
+   }
+
+   public void declareRoles(String... roleNames)
+   {
    }
 }
--- exo.kernel.container/src/test/java/org/exoplatform/container/configuration/TestConfigurationManagerImpl.java
+++ exo.kernel.container/src/test/java/org/exoplatform/container/configuration/TestConfigurationManagerImpl.java
@@ -26,18 +26,27 @@ import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Enumeration;
+import java.util.EventListener;
+import java.util.Map;
 import java.util.Set;
 
+import javax.servlet.Filter;
+import javax.servlet.FilterRegistration;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.Servlet;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+import javax.servlet.ServletRegistration.Dynamic;
+import javax.servlet.SessionCookieConfig;
+import javax.servlet.SessionTrackingMode;
+import javax.servlet.descriptor.JspConfigDescriptor;
 
 /**
  * Created by The eXo Platform SAS
  * Author : Nicolas Filotto 
  *          nicolas.filotto@exoplatform.com
- * 22 fŽvr. 2010  
+ * 22 fï¿½vr. 2010  
  */
 public class TestConfigurationManagerImpl extends TestCase
 {
@@ -461,6 +470,170 @@ public class TestConfigurationManagerImpl extends TestCase
       {
          // TODO Auto-generated method stub
          
+      }
+
+      public int getEffectiveMajorVersion()
+      {
+         // TODO Auto-generated method stub
+         return 0;
+      }
+
+      public int getEffectiveMinorVersion()
+      {
+         // TODO Auto-generated method stub
+         return 0;
+      }
+
+      public boolean setInitParameter(String name, String value)
+      {
+         // TODO Auto-generated method stub
+         return false;
+      }
+
+      public Dynamic addServlet(String servletName, String className)
+      {
+         // TODO Auto-generated method stub
+         return null;
+      }
+
+      public Dynamic addServlet(String servletName, Servlet servlet)
+      {
+         // TODO Auto-generated method stub
+         return null;
+      }
+
+      public Dynamic addServlet(String servletName, Class<? extends Servlet> servletClass)
+      {
+         // TODO Auto-generated method stub
+         return null;
+      }
+
+      public <T extends Servlet> T createServlet(Class<T> clazz) throws ServletException
+      {
+         // TODO Auto-generated method stub
+         return null;
+      }
+
+      @Override
+      public ServletRegistration getServletRegistration(String servletName)
+      {
+         // TODO Auto-generated method stub
+         return null;
+      }
+
+      @Override
+      public Map<String, ? extends ServletRegistration> getServletRegistrations()
+      {
+         // TODO Auto-generated method stub
+         return null;
+      }
+
+      @Override
+      public javax.servlet.FilterRegistration.Dynamic addFilter(String filterName, String className)
+      {
+         // TODO Auto-generated method stub
+         return null;
+      }
+
+      @Override
+      public javax.servlet.FilterRegistration.Dynamic addFilter(String filterName, Filter filter)
+      {
+         // TODO Auto-generated method stub
+         return null;
+      }
+
+      @Override
+      public javax.servlet.FilterRegistration.Dynamic addFilter(String filterName, Class<? extends Filter> filterClass)
+      {
+         // TODO Auto-generated method stub
+         return null;
+      }
+
+      @Override
+      public <T extends Filter> T createFilter(Class<T> clazz) throws ServletException
+      {
+         // TODO Auto-generated method stub
+         return null;
+      }
+
+      @Override
+      public FilterRegistration getFilterRegistration(String filterName)
+      {
+         // TODO Auto-generated method stub
+         return null;
+      }
+
+      @Override
+      public Map<String, ? extends FilterRegistration> getFilterRegistrations()
+      {
+         // TODO Auto-generated method stub
+         return null;
+      }
+
+      public SessionCookieConfig getSessionCookieConfig()
+      {
+         // TODO Auto-generated method stub
+         return null;
+      }
+
+      public void setSessionTrackingModes(Set<SessionTrackingMode> sessionTrackingModes)
+      {
+         // TODO Auto-generated method stub
+
+      }
+
+      public Set<SessionTrackingMode> getDefaultSessionTrackingModes()
+      {
+         // TODO Auto-generated method stub
+         return null;
+      }
+
+      public Set<SessionTrackingMode> getEffectiveSessionTrackingModes()
+      {
+         // TODO Auto-generated method stub
+         return null;
+      }
+
+      public void addListener(String className)
+      {
+         // TODO Auto-generated method stub
+
+      }
+
+      public <T extends EventListener> void addListener(T t)
+      {
+         // TODO Auto-generated method stub
+
+      }
+
+      public void addListener(Class<? extends EventListener> listenerClass)
+      {
+         // TODO Auto-generated method stub
+
+      }
+
+      public <T extends EventListener> T createListener(Class<T> clazz) throws ServletException
+      {
+         // TODO Auto-generated method stub
+         return null;
+      }
+
+      public JspConfigDescriptor getJspConfigDescriptor()
+      {
+         // TODO Auto-generated method stub
+         return null;
+      }
+
+      public ClassLoader getClassLoader()
+      {
+         // TODO Auto-generated method stub
+         return null;
+      }
+
+      public void declareRoles(String... roleNames)
+      {
+         // TODO Auto-generated method stub
+
       }
       
    }
